076eaa6c316f74dd0fa9785bb9b4e84c037e7390
Felipe Rocha de Lucena - FLC <fee.rocha@hotmail.com>
2020-06-12 15:50:52 -0300

    Unidade 4 OK

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from "@angular/core";
+
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
+
+@Component({
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"],
+})
+export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+
+  heroes: Hero[];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+}
